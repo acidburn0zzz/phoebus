2b56ca50f6ec1d21c03903103453ce575cabfac6
Matt A. Tobin <email@mattatobin.com>
2015-05-29 14:20:40 -0400

    Add a switch for using the Client Version whitelist when checking for updates from AMO

--- update/check.php
+++ update/check.php
@@ -13,6 +13,7 @@ $varHardcode_palemoonID = '{8de7fcbb-c55c-4fbe-bfc5-fc555c87dbc4}';
 $varHardcode_firefoxID = '{ec8030f7-c20a-464f-9b0e-13a3a9e97384}';
 $varHardcode_firefoxVersion = '24.9';
 $varAMOKillSwitch = false;
+$varAMOWhitelist = false;
 
 // ============================================================================
 
@@ -72,7 +73,7 @@ elseif (array_key_exists($varRequest_addonID, $arrayLangPackDB)) {
 else {
 	$arrayAllowedAMOVersionDB = array( '25.3.2', '25.4.0', '25.4.1', '99.9.9');
 	if ($varRequest_clientID == $varHardcode_palemoonID) {
-		if ((in_array($varRequest_clientVersion, $arrayAllowedAMOVersionDB) == true) && ($varAMOKillSwitch == false)) {
+		if (($varAMOKillSwitch == false) && ($varAMOWhitelist == false || in_array($varRequest_clientVersion, $arrayAllowedAMOVersionDB) == true)) {
 			$varRequest_reqVersion = $_GET['reqVersion']; // This seems to always be '2'
 			$varRequest_addonCompatMode = $_GET['compatMode']; // This is almost always 'normal' but it can be 'strict' for things like langpacks
 			$varAMOLink = 'https://versioncheck.addons.mozilla.org/update/VersionCheck.php?reqVersion=' . $varRequest_reqVersion . '&id=' . $varRequest_addonID . '&appID=' . $varHardcode_firefoxID . '&appVersion=' . $varHardcode_firefoxVersion . '&compatMode=' . $varRequest_addonCompatMode;
