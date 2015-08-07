528efb75f628532b011331f97749736a002f9efb
Matt A. Tobin <email@mattatobin.com>
2015-08-06 23:57:28 -0400

    [Tools] Update genupdatexml

--- tools/genupdatexml.php
+++ tools/genupdatexml.php
@@ -1,18 +1,33 @@
 <?php
 error_reporting(E_ERROR);
-if(isset($_GET['apmoID'])) {
-	$addonID = $_GET['apmoID'];
+if(isset($_GET['slug'])) {
+	$addonSlug = $_GET['slug'];
 }
 else {
 	die('Error: You need to specify an id');
 }
+if(isset($_GET['type'])) {
+	if($_GET['type'] == 'extension') {
+		$addonType = 'extensions/';
+	}
+	elseif ($_GET['type'] == 'theme') {
+		$addonType = 'themes/';
+	}
+	else {
+		die('Error: Invalid Type');
+	}
+	
+}
+else {
+	die('Error: You need to specify a type');
+}
 
-$addon_manifest = parse_ini_file($_SERVER["DOCUMENT_ROOT"] . '/phoebus/datastore/' . $addonID . '/manifest.ini');
+$addon_manifest = parse_ini_file($_SERVER["DOCUMENT_ROOT"] . '/phoebus/datastore/' . $addonType . $addonSlug . '/manifest.ini');
 if ($addon_manifest == false) {
 die('Error: Unable to read manifest ini file');
 }
 else {
-$xpiHash = hash_file('sha256', $_SERVER["DOCUMENT_ROOT"] . '/phoebus/datastore/' . $addon_manifest["id"] . '/' . $addon_manifest["xpi"]);
+$xpiHash = hash_file('sha256', $_SERVER["DOCUMENT_ROOT"] . '/phoebus/datastore/' . $addonType . $addonSlug . '/' . $addon_manifest["xpi"]);
 
 $updateWriteOut ='<?xml version="1.0" encoding="UTF-8"?>
 
@@ -30,7 +45,7 @@ $updateWriteOut ='<?xml version="1.0" encoding="UTF-8"?>
                 <em:id>{8de7fcbb-c55c-4fbe-bfc5-fc555c87dbc4}</em:id>
                 <em:minVersion>25.0</em:minVersion>
                 <em:maxVersion>' . $addon_manifest["compat"] . '</em:maxVersion>
-                <em:updateLink>https://addons.palemoon.org/phoebus/datastore/' . $addon_manifest["id"] . '/' . $addon_manifest["xpi"] . '</em:updateLink>
+                <em:updateLink>https://addons.palemoon.org/phoebus/datastore/' . $addonType . $addonSlug . '/' . $addon_manifest["xpi"] . '</em:updateLink>
                 <em:updateHash>sha256:' . $xpiHash . '</em:updateHash>
               </RDF:Description>
             </em:targetApplication>
