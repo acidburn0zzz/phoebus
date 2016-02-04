f829058d4582f07f2d91a6a76ac9cf7a56f86557
Antonius32 <Antonius32@users.noreply.github.com>
2016-02-04 04:45:04 +0100

    [AUS] Add Open about:config

--- services/aus/database.php
+++ services/aus/database.php
@@ -23,7 +23,8 @@
 		'{2db74bf0-e2ce-4412-a47c-ec6de1449db1}' => 'space-advance',
 		'{f60dcfb5-358d-498e-9f2e-1b53eba4dde7}' => 'searchload-options-revived',
 		'jid1-IDCACPalemoon@jetpack' => 'i-dont-care-about-cookies',
-		'mozarchiver@lootyhoof-pm' => 'mozarchiver'
+		'mozarchiver@lootyhoof-pm' => 'mozarchiver',
+		'{98a2ae55-3a75-4354-a460-9176594d05c3}' => 'open-about-config'
 	);
 	
 	$arrayThemesDB = array(
