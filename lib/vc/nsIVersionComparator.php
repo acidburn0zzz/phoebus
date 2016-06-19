91a9a37fa57c1745ec5e1427238f3d407f26c24a
Matt A. Tobin <email@mattatobin.com>
2016-06-19 17:38:44 -0400

    Add a tool to easily check toolkit-style versions

--- lib/vc/nsIVersionComparator.php
+++ lib/vc/nsIVersionComparator.php
@@ -3,65 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-
-//
-// Unit tests
-//
-// (compare results to version order in specifications below)
-//
-/*
-$all = array('1.0pre1', '1.0pre2', '1.0', '1.0.0', '1.0.0.0', '1.1pre', '1.1pre0', '1.0+',
-	'1.1pre1a', '1.1pre1', '1.1pre10a', '1.1pre10');
-
-for ($i=0; $i<sizeOf($all); $i++) {
-	for ($j=0; $j<sizeOf($all); $j++) {
-		$a = $all[$i];
-		$b = $all[$j];
-		$compare = ToolkitVersionComparator::compare($a, $b);
-		if ($compare<0) {
-			echo $a . ' is less than ' . $b;
-		}
-		if ($compare==0) {
-			echo $a . ' equals ' . $b;
-		}
-		if ($compare>0) {
-			echo $a . ' is greater than ' . $b;
-		}
-		echo "\n";
-	}
-	echo "\n\n";
-}
-*/
-
-
-/**
- * Implements Mozilla Toolkit's nsIVersionComparator
- *
- * Version strings are dot-separated sequences of version-parts.
- *
- * A version-part consists of up to four parts, all of which are optional:
- * <number-a><string-b><number-c><string-d (everything else)>
- * A version-part may also consist of a single asterisk "*" which indicates
- * "infinity".
- *
- * Numbers are base-10, and are zero if left out.
- * Strings are compared bytewise.
- *
- * For additional backwards compatibility, if "string-b" is "+" then
- * "number-a" is incremented by 1 and "string-b" becomes "pre".
- *
- * 1.0pre1
- * < 1.0pre2  
- *   < 1.0 == 1.0.0 == 1.0.0.0
- *     < 1.1pre == 1.1pre0 == 1.0+
- *       < 1.1pre1a
- *         < 1.1pre1
- *           < 1.1pre10a
- *             < 1.1pre10
- *
- * Although not required by this interface, it is recommended that
- * numbers remain within the limits of a signed char, i.e. -127 to 128.
- */
 class ToolkitVersionComparator {
 	public static function compare($a, $b) {
 		do {
---
+++ tools/ezvc.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Easy nsIVersionComparator</title>
+	</head>
+	<body>
+		<p>Check the result of a services.vc comparison.
+        <form action="ezvc.php" method="post">
+            Current Version: <input type="text" name="currVersion"><br>
+            Compare to Version: <input type="text" name="compVersion"><br>
+            <input type="submit">
+        </form>
+        <hr>
+        <?php
+            if (isset($_POST[currVersion]) && isset($_POST[compVersion])) {
+                require_once('../lib/vc/nsIVersionComparator.php')
+                
+                $vcResult = ToolkitVersionComparator::compare($_POST[currVersion], $_POST[compVersion]);
+                
+                if ($vcResult = 0) {
+                    $htmlWriteOut = '<p>Compare version ' . $_POST[compVersion] . ' is the same as current version ' . $_POST[currVersion] . '</p>';
+                }
+                elseif ($vcResult = 1) {
+                    $htmlWriteOut = '<p>Compare version' . $_POST[compVersion] . ' is newer than current version ' . $_POST[currVersion] . '</p>';
+                }
+                elseif ($vcResult = -1) {
+                    $htmlWriteOut = '<p>Compare version' . $_POST[compVersion] . ' is older than current version ' . $_POST[currVersion] . '</p>';
+                }
+                print($htmlWriteOut);
+            }
+        ?>
+	</body>
+</html>
