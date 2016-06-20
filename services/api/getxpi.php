93eb7f6bb9f048ab06b26e0281d1baf2c2c23b06
Matt A. Tobin <email@mattatobin.com>
2016-06-20 03:02:22 -0400

    [API] GetXPI: Correct missing ';'

--- services/api/getxpi.php
+++ services/api/getxpi.php
@@ -96,7 +96,7 @@ function funcGetXPI($varAddonType, $varAddonData) {
 			$varDownloadLink = $varBaseURL . $varAddonData . '/' . $addonManifest["xpi"];
 		}
         
-        funcRedirect($varDownloadLink)
+        funcRedirect($varDownloadLink);
     }
 }
 
