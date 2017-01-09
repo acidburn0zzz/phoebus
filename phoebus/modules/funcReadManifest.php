f3adcfd61f1497ffa4716b999f4103c00f0f3182
Matt A. Tobin <email@mattatobin.com>
2017-01-09 11:35:36 -0500

    Increase to 102

--- phoebus/modules/funcReadManifest.php
+++ phoebus/modules/funcReadManifest.php
@@ -34,7 +34,7 @@ function funcReadManifest($_addonType, $_addonSlug, $_addonMetadata, $_addonCont
             $_addonManifest['metadata']['shortDescription'] = htmlentities($_addonManifest['metadata']['shortDescription'], ENT_XHTML);
             $_addonFullShortDesc = $_addonManifest['metadata']['shortDescription'];
             if (strlen($_addonManifest['metadata']['shortDescription']) >= 103) {
-                $_addonManifest['metadata']['shortDescription'] = substr($_addonManifest['metadata']['shortDescription'], 0, 100) . '...';
+                $_addonManifest['metadata']['shortDescription'] = substr($_addonManifest['metadata']['shortDescription'], 0, 102) . '...';
             }
         }
         else {
