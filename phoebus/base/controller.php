67467d01b3b3d3ea49c26bc875d5243b7bce4080
Matt A. Tobin <email@mattatobin.com>
2017-01-31 11:29:15 -0500

    More GUIDs

--- phoebus/base/controller.php
+++ phoebus/base/controller.php
@@ -13,7 +13,10 @@ $strPhoebusVersion = '1.5.0a1';
 
 $strPaleMoonID = '{8de7fcbb-c55c-4fbe-bfc5-fc555c87dbc4}';
 $strFirefoxID = '{ec8030f7-c20a-464f-9b0e-13a3a9e97384}';
+$strThunderbirdID = '{3550f703-e582-4d05-9a08-453d09bdfdc6}';
+$strSeaMonkeyID = '{92650c4d-4b8e-4d2a-b7eb-24ecf4f6b63a}';
 $strApplicationID = $strPaleMoonID;
+
 $strMinimumApplicationVersion = '27.0.0';
 $strFirefoxVersion = '27.9';
 $strFirefoxOldVersion = '24.9';
