9401dbbe7743af623d68f92ed3b24ad18f8102eb
Matt A. Tobin <email@mattatobin.com>
2017-03-21 15:14:47 -0400

    Deprecate [img] from phoebuscode

--- phoebus/modules/funcProcessContent.php
+++ phoebus/modules/funcProcessContent.php
@@ -33,7 +33,7 @@ function funcProcessContent ($_input) {
             '\<(ul|\/ul|li|\/li|p|\/p)\><br \/>' => '<$1>',
             '\[url=(.*)\](.*)\[\/url\]' => '<a href="$1" target="_blank">$2</a>',
             '\[url\](.*)\[\/url\]' => '<a href="$1" target="_blank">$1</a>',
-            '\[img(.*)\](.*)\[\/img\]' => '<img src="$2"$1 />'
+            '\[img(.*)\](.*)\[\/img\]' => '<small>The [img] tag is deprecated please use preview.png.</small>'
         );
         
         // str replace pseudo-bbcode with real html
